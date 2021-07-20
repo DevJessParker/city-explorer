39d66d97b03b4303e13457f1e5db2524f57926f9
DevJessParker <jess.k.dempsey@gmail.com>
2021-07-19 17:34:25 -0700

    Linked API and created input forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery = '',
+      location = {}
+    }
+  }
+
+  getLocation = async () => {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.searchQuery}&format=json`;
+    const response = await axios.get(API);
+
+    this.setState({ location: response.data[0] })
+
+  }
+
+  render() {
+    return (
+      <>
+        <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="Enter City Name" type="text" />
+        <button onClick={this.getLocation}>Explore!</button>
+
+        <p>Location: {this.state.location.display_name}</p>
+        <p>Lat: {this.state.location.lat}</p>
+        <p>Lon: {this.state.location.lon}</p>
+        <span>{this.state.location.icon}</span>
+      </>
+    )
+  }
 }
 
 export default App;
